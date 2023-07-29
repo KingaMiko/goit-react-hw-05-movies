03e7a8d31806f98068b7926eadd74a4007f51b9a
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2023-07-29 15:13:04 +0200

    Home page with trending movies added

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useParams } from 'react-router-dom';
 
 export function MovieDetails() {
+  const { id } = useParams();
   return (
     <div>
       <h1> MovieDetails Page</h1>
