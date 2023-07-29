f18161410f7dc8d2f15e0b5dd45906c492be9bf8
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2023-07-29 14:54:46 +0200

    Components, first style

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
 
 export function MovieDetails() {
   return (
-    <>
-      <div>MovieDetails PAGE</div>
-    </>
+    <div>
+      <h1> MovieDetails Page</h1>
+      <ul>
+        <li>
+          <Link to="Cast">Cast</Link>
+        </li>
+        <li>
+          <Link to="Reviews"> Go to Reviews</Link>
+        </li>
+      </ul>
+      <Outlet />
+    </div>
   );
 }
