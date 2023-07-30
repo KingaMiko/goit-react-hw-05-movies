7f26127e7105b87c030ae552bb538d1cfa2b8dfa
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2023-07-30 09:59:57 +0200

    deploy fix

--- src/Api/fetchMovies.js
+++ src/Api/fetchMovies.js
@@ -7,6 +7,5 @@ export function fetchMovies(url) {
         'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZDhlY2Y3NDVmNWJiZTUwZmM2NDhjMDg1OWZhMTcwMSIsInN1YiI6IjY0ODhjMTU2ZDJiMjA5MDBjYTIxNzA5NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4vxGV4kBszJbv90PYcnQ1DUyBmnKxmAR_P1khjcXgUk',
     },
   };
-
   return fetch(url, options).then(res => res.json());
 }
