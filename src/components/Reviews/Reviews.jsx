446e7a9df5017140277bd615ad67cb5225e866db
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2023-09-14 23:55:45 +0200

    fix fetchMovies import

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { fetchMovieReviews } from 'api/fetchMovies';
+import { fetchMovieReviews } from 'api/fetchMovies.js';
 
 function Reviews() {
   const { movieId } = useParams();
